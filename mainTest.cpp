f41cf61c8e641def897e84555fa6a1f71fdee4c2
Fritz Velarde-Alvarez <fritz.velardealvarez.aguilar@ucsp.edu.pe>
2017-07-10 15:33:20 -0500

    Add files via upload

--- mainTest.cpp
+++ mainTest.cpp
@@ -10,23 +10,60 @@ using namespace std;
 
 int main()
 {
+    cGraph<string,int> grafoMain;
     cGraph<string,int> grafo;
-    cGraph<string,int> grafo2;
-	cTree<string,int> arbol;
+    cTree<string,int> arbol;
 	
+
+    grafoMain.insertNode("primer commit");
+    grafoMain.insertNode("segundo commit");
+    grafoMain.insertNode("tercer commit");
+    grafoMain.insertNode("cuarto commit");
+    grafoMain.insertNode("quinto commit");
+    grafoMain.insertNode("sexto commit");
+    grafoMain.insertNode("primer commit");
+    
     grafo.insertNode("primer commit");
+	cout<<"------Tiempo1_nodos:"<<endl;
+    for(int i=0; i<grafo.mNodes.size();i++){
+        cout<<grafo.mNodes[i]->mData<<endl;
+    }
     grafo.insertNode("segundo commit");
+    
+	cout<<"------Tiempo2_nodos:"<<endl;
+    for(int i=0; i<grafo.mNodes.size();i++){
+        cout<<grafo.mNodes[i]->mData<<endl;
+    }
     grafo.insertNode("tercer commit");
-    grafo.insertNode("cuarto commit");
-    grafo.insertNode("quinto commit");
-    grafo.insertNode("sexto commit");
-    grafo.insertEdge(1,"primer commit","segundo commit",0);
-    grafo.insertEdge(2,"tercer commit","cuarto commit",1);
-    grafo.insertEdge(3,"sexto commit","segundo commit",1);
-    grafo.insertEdge(4,"quinto commit","tercer commit",1);
-    grafo.insertEdge(5,"segundo commit","quinto commit",1);
 	
-	cout<<"------nodos:"<<endl;
+	cout<<"------Tiempo3_nodos:"<<endl;
+    for(int i=0; i<grafo.mNodes.size();i++){
+        cout<<grafo.mNodes[i]->mData<<endl;
+    }
+
+    grafo.insertNode("cuarto commit");
+	cout<<"------Tiempo4_nodos:"<<endl;
+    for(int i=0; i<grafo.mNodes.size();i++){
+        cout<<grafo.mNodes[i]->mData<<endl;
+    }
+
+    grafo.insertNode("quinto commit");
+	cout<<"------Tiempo5_nodos:"<<endl;
+    for(int i=0; i<grafo.mNodes.size();i++){
+        cout<<grafo.mNodes[i]->mData<<endl;
+    }
+    grafo.insertNode("sexto commit");
+	cout<<"------Tiempo6_nodos:"<<endl;
+    for(int i=0; i<grafo.mNodes.size();i++){
+        cout<<grafo.mNodes[i]->mData<<endl;
+    }
+    grafo.insertEdge(1,"primer commit","segundo commit",0);
+    grafo.insertEdge(2,"segundo commit","tercer commit",0);
+    grafo.insertEdge(3,"tercer commit","cuarto commit",0);
+    grafo.insertEdge(4,"cuarto commit","quinto commit",0);
+    grafo.insertEdge(5,"quinto commit","sexto commit",0);
+	
+/*	cout<<"------nodos:"<<endl;
     for(int i=0; i<grafo.mNodes.size();i++){
         cout<<grafo.mNodes[i]->mData<<endl;
     }
@@ -36,12 +73,12 @@ int main()
     	cout<<"Arista "<<grafo.mEdges[i]->mData<<" Nodos: "<<grafo.mEdges[i]->m_node[0]->mData<<" - "<<grafo.mEdges[i]->m_node[1]->mData<<endl;
 		if(!grafo.mEdges[i]->m_dir)
 		cout<<"Arista "<<grafo.mEdges[i]->mData<<" Nodos: "<<grafo.mEdges[i]->m_node[1]->mData<<" - "<<grafo.mEdges[i]->m_node[0]->mData<<endl;
-	}
+	}*/
     
 	grafo.removeEdge(1,"primer commit","segundo commit");
     grafo.removeNode("primer commit");
     
-	cout<<"------nodos:"<<endl;
+/*	cout<<"------nodos:"<<endl;
     for(int i=0; i<grafo.mNodes.size();i++){
         cout<<grafo.mNodes[i]->mData<<endl;
     }
@@ -51,14 +88,14 @@ int main()
     	cout<<"Arista "<<grafo.mEdges[i]->mData<<" Nodos: "<<grafo.mEdges[i]->m_node[0]->mData<<" - "<<grafo.mEdges[i]->m_node[1]->mData<<endl;
 		if(!grafo.mEdges[i]->m_dir)
 		cout<<"Arista "<<grafo.mEdges[i]->mData<<" Nodos: "<<grafo.mEdges[i]->m_node[1]->mData<<" - "<<grafo.mEdges[i]->m_node[0]->mData<<endl;
-	}
+	}*/
 	
 	cout<<grafo.mNodes.size()<<endl<<endl;
 
-	for(int i=0; i<grafo.mNodes.size();i++){
+	for(int i=300; i<grafo.mNodes.size()+300;i++){
 		// cNode<cGraph<string,int>>* ptr;
-		grafo.mNodes[i]->dia=i;
-		arbol.insert2(grafo.mNodes[i]);
+		grafo.mNodes[i-300]->dia=i;
+		arbol.insert2(grafo.mNodes[i-300]);
 	}
 	cout<<"impresion inorden "<<endl;
 	arbol.printInOrden(arbol.mRoot);
